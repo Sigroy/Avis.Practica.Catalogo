9c70740a98a5edaeee0b8186529e4025a3722e35
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-10-03 11:21:17 -0500

    Terminar de implementar el AutoAggregate

--- Layers/Infrastructure/Aggregates/AutoAggregate.cs
+++ Layers/Infrastructure/Aggregates/AutoAggregate.cs
@@ -1,12 +1,9 @@
 ﻿using AVIS.CoreBase.Clases;
 using AVIS.CoreBase.Extensions;
-
 using FluentValidation.Results;
 using FluentValidation;
-
 using Avis.Catalogo.Application;
 using Avis.Catalogo.Domain;
-using MongoDB.Driver.Core.Events;
 
 namespace Avis.Catalogo.Infrastructure;
 
@@ -22,7 +19,8 @@ public class AutoAggregate : IAutoAggregate
 
     public bool Success { get; private set; } = false;
 
-    public AutoAggregate(IValidator<AutoDTO> validator, IDapperUnitofWork unitofWork)
+    public AutoAggregate(IValidator<AutoDTO> validator,
+        IDapperUnitofWork unitofWork)
     {
         _unitofWork = unitofWork;
         _validator = validator;
@@ -46,21 +44,79 @@ public class AutoAggregate : IAutoAggregate
                 _auto = auto.ToModelorVM<Auto>();
                 var key = Guid.NewGuid().ToString();
                 _auto.AutoPkey = key;
-                
+
                 #region TRANSACCION DAPPER
 
                 try
                 {
-                    id = await _unitofWork.ResCmdRepository.AddAsync(_auto);
+                    id = await _unitofWork.AutoCmdRepository.AddAsync(_auto);
 
-                    if (_unitofWork.ResCmdRepository.Success)
+                    if (_unitofWork.AutoCmdRepository.Success)
                     {
-                        
+                        if (_unitofWork.AutoCmdRepository.Success)
+                        {
+                            Success = true;
+                        }
+                    }
+
+                    if (Success)
+                    {
+                        _unitofWork.Commit();
+                    }
+                    else
+                    {
+                        id = 0;
+                        Success = false;
+
+                        if (_unitofWork.AutoCmdRepository.Errores.Count > 0)
+                        {
+                            _unitofWork.AutoCmdRepository.Errores.ToCurrentList(Errores);
+                        }
+
+                        _unitofWork.Rollback();
                     }
                 }
+                catch (Exception ex)
+                {
+                    Success = false;
+                    string extra = "";
+                    if (ex.InnerException != null)
+                    {
+                        extra = ex.InnerException.Message;
+                    }
+
+                    InternalException error = new InternalException()
+                    {
+                        ClassName = this.GetType().ToString(),
+                        MethodName = "Transaccion Dapper",
+                        ErrorMessage = "Inner:" + extra + " Exception:" + ex.Message,
+                        Ex = ex
+                    };
+                    Errores.Add(error);
+                }
+
                 #endregion
             }
         }
-    }
+        catch (Exception ex)
+        {
+            Success = false;
+            string extra = "";
+            if (ex.InnerException != null)
+            {
+                extra = ex.InnerException.Message;
+            }
 
+            InternalException error = new InternalException()
+            {
+                ClassName = this.GetType().ToString(),
+                MethodName = "Add",
+                ErrorMessage = "Inner:" + extra + " Exception:" + ex.Message,
+                Ex = ex
+            };
+            Errores.Add(error);
+        }
+
+        return id;
+    }
 }
